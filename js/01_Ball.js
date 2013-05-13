2afc890e61334f93170ebe58d8002a1e7705ca3d
Sohei Kitada <sohei@td-inc.jp>
2013-05-13 21:12:07 +0900

    canvasへのアクセス

--- js/01_Ball.js
+++ js/01_Ball.js
@@ -2,4 +2,9 @@ jQuery(function($){
 
   console.log("01_Ball start");
 
+  // canvas要素のノードオブジェクト
+  var canvas = document.getElementById('01_Ball_canvas');
+  // 2Dコンテキスト
+  var ctx = canvas.getContext('2d');
+
 });
