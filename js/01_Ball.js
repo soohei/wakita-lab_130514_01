ec35e4e2b307fd6808f292e8c105d438180aa98b
Sohei Kitada <sohei@td-inc.jp>
2013-05-13 21:26:24 +0900

    四角形を描く

--- js/01_Ball.js
+++ js/01_Ball.js
@@ -7,4 +7,13 @@ jQuery(function($){
   // 2Dコンテキスト
   var ctx = canvas.getContext('2d');
 
+
+  // 四角形を描く
+  ctx.beginPath();
+  ctx.moveTo(20, 20);
+  ctx.lineTo(120, 20);
+  ctx.lineTo(120, 120);
+  ctx.lineTo(20, 120);
+  ctx.closePath();
+  ctx.stroke();
 });
