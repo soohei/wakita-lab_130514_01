40c615444b802698644d2b7f481e1520eaa8c7f4
Sohei Kitada <sohei@td-inc.jp>
2013-05-13 22:03:20 +0900

    円のパラメーターをインスタンス生成時に指定

--- js/01_Ball.js
+++ js/01_Ball.js
@@ -8,7 +8,7 @@ jQuery(function($){
   var ctx = canvas.getContext('2d');
 
   // Ballインスタンスを生成
-  var ball = new Ball(ctx);
+  var ball = new Ball(ctx, 70, 70, 50, '#ffffff', '#333333');
 });
 
 ////////////////////////////////
@@ -16,13 +16,19 @@ jQuery(function($){
 // 参考: http://soohei.net/441
 ////////////////////////////////
 
-var Ball = function(ctx){
+var Ball = function(ctx, x, y, radius, strokeColor, fillColor){
   this.initialize.apply(this, arguments);
 }
 Ball.prototype = {
   //コンストラクタ
-  initialize: function(ctx) {
+  initialize: function(ctx, x, y, radius, strokeColor, fillColor) {
     this.ctx = ctx;
+    this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.strokeColor = strokeColor;
+    this.fillColor = fillColor;
     this.draw();
   }
   ,
@@ -32,11 +38,11 @@ Ball.prototype = {
     // 円を描く
 
     // 色の指定
-    ctx.strokeStyle = '#ffffff';
-    ctx.fillStyle = '#333333';
+    ctx.strokeStyle = this.strokeColor;
+    ctx.fillStyle = this.fillColor;
 
     ctx.beginPath();
-    ctx.arc(70, 70, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
