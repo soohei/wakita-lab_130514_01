510f31a3d8059d0f1741651b01819aeab41b63d0
Sohei Kitada <sohei@td-inc.jp>
2013-05-13 21:28:47 +0900

    円を描画

--- js/01_Ball.js
+++ js/01_Ball.js
@@ -8,12 +8,15 @@ jQuery(function($){
   var ctx = canvas.getContext('2d');
 
 
-  // 四角形を描く
+  // 円を描く
 
   // 色の指定
   ctx.strokeStyle = '#ffffff';
   ctx.fillStyle = '#333333';
 
-  ctx.fillRect(20, 20, 100, 100);
-  ctx.strokeRect(20, 20, 100, 100);
+  ctx.beginPath();
+  ctx.arc(70, 70, 50, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
 });
