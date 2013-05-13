21170068f99f9c9411dd04e0ac9fc18813feb7e1
Sohei Kitada <sohei@td-inc.jp>
2013-05-13 21:27:46 +0900

    描画処理を簡略化

--- js/01_Ball.js
+++ js/01_Ball.js
@@ -14,12 +14,6 @@ jQuery(function($){
   ctx.strokeStyle = '#ffffff';
   ctx.fillStyle = '#333333';
 
-  ctx.beginPath();
-  ctx.moveTo(20, 20);
-  ctx.lineTo(120, 20);
-  ctx.lineTo(120, 120);
-  ctx.lineTo(20, 120);
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke();
+  ctx.fillRect(20, 20, 100, 100);
+  ctx.strokeRect(20, 20, 100, 100);
 });
